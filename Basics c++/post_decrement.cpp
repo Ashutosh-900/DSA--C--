08c795f60bd90c19e4448f599b289349c33440a8
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-07-20 21:41:47 +0530

    Adding more pattern and for loop programs

---
+++ Basics c++/post_decrement.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a = 2;
+    int i = 3;
+   int sum = a + (i--);
+   cout<< i <<endl;
+   cout << sum <<endl;
+}
---
+++ Basics c++/post_increment.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int a = 2;
+    int i = 3;
+   int sum = a + (i++);
+   cout<< sum <<endl;
+   cout<< i <<endl;
+
+}
