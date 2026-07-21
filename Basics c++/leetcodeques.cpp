4b09728db5d41fd02246d36cd650acd582cf3c7e
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-07-21 20:21:54 +0530

    practicing leetcode ques

---
+++ Basics c++/leetcodeques.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int prod= 1;
+    int sum = 0;
+    int n;
+    cin>>n;
+    while(n!=0){
+        int digit= n%10;
+        prod =prod*digit;
+        sum = sum + digit;
+        n= n /10;
+    }
+     int answer = prod -sum ;
+     cout<< answer;
+}
