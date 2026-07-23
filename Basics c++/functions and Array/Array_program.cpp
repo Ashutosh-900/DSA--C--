e02640282f26be4a77bc5248ef3e7084056852be
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-07-23 22:06:39 +0530

    Adding simple array program

---
+++ Basics c++/functions and Array/Array_program.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int number[15];
+    cout<< "value at 0 index : "<< number[1]<<endl;
+    cout<< endl << "program is running"<< endl;
+
+    cout<<endl;
+
+    int second[3]= {3,15,-6};
+    cout<< "value at 2 index is :"<< second[2] <<endl;
+}
