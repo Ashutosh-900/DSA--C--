7506a290232bde6170c53f421fc3488eba8cc0c3
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-07-25 21:49:34 +0530

    Array program 3

---
+++ Basics c++/functions and Array/characterdatatype_array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+char ch[7] = {'a','b','c','d','g','z','j'};
+   cout<<ch[5]<<endl;
+   cout<<"printing an array"<<endl;
+   for(int i=0 ; i<7 ; i++){
+    cout<< ch[i]<<" ";
+    
+   }
+}
