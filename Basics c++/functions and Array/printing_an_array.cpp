c06726f4d524d21855df0b261ea28560226f7f9d
Ashutosh-900 <ashutoshkumar2364@gmail.com>
2026-07-25 21:48:45 +0530

    Array program 1

---
+++ Basics c++/functions and Array/printing_an_array.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int n = 15;
+    int DSA[15]={2,7};
+    cout<< "printing an array"<< endl;
+    for(int i=0 ; i<n ; i++){
+        cout<< DSA[i]<< " ";
+    }
+}   
+
+
